55230a2c2ea96d4aedbdd725a9992f69f7713f6c
daijahm19 <dmcdon43@students.kennesaw.edu>
2025-04-14 22:06:20 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // send HTML file on GET request
+});
+
+app.post('/submit-form', (req, res) => {
+    const fname = req.body.fname;
+    res.send(`Thank you ${fname}, we will get back to you shortly.`);
+});
+    
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
